f1218a5ede6922c9b2d4c82d8fc770e8b65a6868
isarvangydv <sarvangyadav886@gmail.com>
2024-10-18 16:22:23 +0530

    htmldhg

---
+++ js work/index.js
@@ -0,0 +1,15 @@
+// console.log("Hello");
+
+// var a=12;
+// if(a>10){
+//    let b=20;
+//    a=40;
+//    console.log("a inside:"+a);
+// }
+// console.log("a outside:"+a);
+function fun(a,b){
+    console.log("Hiii")
+    return a+b;
+}
+const result = fun();
+console.log(result);

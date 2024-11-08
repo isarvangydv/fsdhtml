103c5f9eb60627e56f0f43d9a6ccab59269ce2e7
isarvangydv <sarvangyadav886@gmail.com>
2024-11-08 11:48:17 +0530

    js callback done

--- js work/index.js
+++ js work/index.js
@@ -7,9 +7,35 @@
 //    console.log("a inside:"+a);
 // }
 // console.log("a outside:"+a);
+console.log("Hi, inside index.js")
 function fun(a,b){
-    console.log("Hiii")
+    console.log("Hiiii");
     return a+b;
 }
-const result = fun();
+const result = fun(23,12);
 console.log(result);
+function cCompiler()
+{
+    return "C Compiler";
+}
+
+function javaCompiler(){
+    return "Java Compiler";
+}
+
+function selectLanguage(language){
+    let data;
+    if(language=='c'){
+    function cCompiler()
+}
+if(language=="java"){
+    function javaCompiler(){
+     return "Java Compiler";
+    }
+data=javaCompiler();
+}
+   return data;
+return "Hi, you have selected: "+clbk();
+}
+
+console.log(selectLanguage(javaCompiler));
